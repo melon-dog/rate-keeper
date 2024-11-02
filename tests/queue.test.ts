f5ec58b09902b00329be22f887b4204a801b5539
melon-dog <JackTezos@proton.me>
2024-11-02 08:36:26 +0100

    🧪: Update 'Basic Usage' test

--- tests/queue.test.ts
+++ tests/queue.test.ts
@@ -47,31 +47,31 @@ test('Basic Usage', async () => {
   actions.push(logger1Queue1("[Q1-200ms-2]"));
   actions.push(logger1Queue1("[Q1-200ms-3]"));
 
-  actions.push(logger2Queue2("[Q2-100ms-1]"));
+  actions.push(logger2Queue2("[Q2-100ms-1]").then(x => unsafeLogger(x + " promise")));
   actions.push(logger2Queue2("[Q2-100ms-2]"));
   actions.push(logger2Queue2("[Q2-100ms-3]"));
 
-  actions.push(logger3Queue2("[Q2-100ms-4]"));
+  actions.push(logger3Queue2("[Q2-100ms-4]").then(x => unsafeLogger(x + " promise")));
   actions.push(logger3Queue2("[Q2-100ms-5]"));
   actions.push(logger3Queue2("[Q2-100ms-6]"));
 
   await Promise.all(actions);
   expect(log).toStrictEqual([
-    "[OQ-500ms-1]", //0ms
-    "[US-1]",       //0ms
-    "[US-2]",       //0ms
-    "[US-3]",       //0ms
-    "[Q1-200ms-1]", //0ms
-    "[Q2-100ms-1]", //0ms
-    "[Q2-100ms-2]", //100ms
-    "[Q1-200ms-2]", //200ms
-    "[Q2-100ms-3]", //200ms
-    "[Q2-100ms-4]", //300ms
-    "[Q1-200ms-3]", //400ms
-    "[Q2-100ms-5]", //400ms
-    "[OQ-500ms-2]", //500ms
-    "[Q2-100ms-6]", //500ms
-    "[OQ-500ms-3]", //1000ms
+    "[OQ-500ms-1]",         //0ms
+    "[US-1]",               //0ms
+    "[US-2]",               //0ms
+    "[US-3]",               //0ms
+    "[Q1-200ms-1]",         //0ms
+    "[Q2-100ms-1] promise", //0ms
+    "[Q2-100ms-2]",         //100ms
+    "[Q1-200ms-2]",         //200ms
+    "[Q2-100ms-3]",         //200ms
+    "[Q2-100ms-4] promise", //300ms
+    "[Q1-200ms-3]",         //400ms
+    "[Q2-100ms-5]",         //400ms
+    "[OQ-500ms-2]",         //500ms
+    "[Q2-100ms-6]",         //500ms
+    "[OQ-500ms-3]",         //1000ms
   ]);
 });
 

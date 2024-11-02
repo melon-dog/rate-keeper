04e4a94cce54c175792066875820cb8ddc76cfbd
melon-dog <JackTezos@proton.me>
2024-11-02 08:05:19 +0100

    🧪 Drop Policy Reject Test

--- tests/queue.test.ts
+++ tests/queue.test.ts
@@ -1,5 +1,5 @@
-import { expect, test, vi } from 'vitest'
-import RateKeeper from "../src/index";
+import { afterEach, expect, test, vi } from 'vitest'
+import RateKeeper, { DropPolicy } from "../src/index";
 
 let log: string[] = [];
 
@@ -7,41 +7,53 @@ function ResetLog() {
   log = [];
 }
 
-function UnsafeLogger(newLog: string) {
+function unsafeLogger(newLog: string) {
   log.push(newLog);
+  return newLog;
 }
 
-const SafeLoggerOwnQueue = RateKeeper(UnsafeLogger, 500); // 500ms
-const SafeLoggerQueue1 = RateKeeper(UnsafeLogger, 200, { id: 1 }); // 200ms
+const loggerIndependant = RateKeeper(unsafeLogger, 500); // 500ms
+const logger1Queue1 = RateKeeper(unsafeLogger, 200, { id: 1 }); // 200ms
+const logger2Queue2 = RateKeeper(unsafeLogger, 100, { id: 2 }); // 100ms
+const logger3Queue2 = RateKeeper(unsafeLogger, 100, { id: 2 }); // 100ms
+const logger4Queue3Reject = RateKeeper(unsafeLogger, 300, {
+  id: 3,
+  dropPolicy: DropPolicy.reject,
+  maxQueueSize: 4
+}); // 300ms
+const logger5Queue4Oldest = RateKeeper(unsafeLogger, 300, {
+  id: 4,
+  dropPolicy: DropPolicy.dropOldest,
+  maxQueueSize: 8
+}); // 300ms
 
-const SafeLoggerQueue2 = RateKeeper(UnsafeLogger, 100, { id: 2 }); // 100ms
-
-const AnotherSafeLoggerQueue2 = RateKeeper(UnsafeLogger, 100, { id: 2 }); // 100ms
-
-test('RateKeeper Queue', async () => {
-
-  const actions: Promise<void>[] = [];
+afterEach(() => {
   ResetLog();
+});
 
-  actions.push(SafeLoggerOwnQueue("[OQ-500ms-1]"));
-  actions.push(SafeLoggerOwnQueue("[OQ-500ms-2]"));
-  actions.push(SafeLoggerOwnQueue("[OQ-500ms-3]"));
+test('Basic Usage', async () => {
 
-  UnsafeLogger("[US-1]");
-  UnsafeLogger("[US-2]");
-  UnsafeLogger("[US-3]");
+  const actions: Promise<string>[] = [];
 
-  actions.push(SafeLoggerQueue1("[Q1-200ms-1]"));
-  actions.push(SafeLoggerQueue1("[Q1-200ms-2]"));
-  actions.push(SafeLoggerQueue1("[Q1-200ms-3]"));
+  actions.push(loggerIndependant("[OQ-500ms-1]"));
+  actions.push(loggerIndependant("[OQ-500ms-2]"));
+  actions.push(loggerIndependant("[OQ-500ms-3]"));
 
-  actions.push(SafeLoggerQueue2("[Q2-100ms-1]"));
-  actions.push(SafeLoggerQueue2("[Q2-100ms-2]"));
-  actions.push(SafeLoggerQueue2("[Q2-100ms-3]"));
+  unsafeLogger("[US-1]");
+  unsafeLogger("[US-2]");
+  unsafeLogger("[US-3]");
 
-  actions.push(AnotherSafeLoggerQueue2("[Q2-100ms-4]"));
-  actions.push(AnotherSafeLoggerQueue2("[Q2-100ms-5]"));
-  actions.push(AnotherSafeLoggerQueue2("[Q2-100ms-6]"));
+  actions.push(logger1Queue1("[Q1-200ms-1]"));
+  actions.push(logger1Queue1("[Q1-200ms-2]"));
+  actions.push(logger1Queue1("[Q1-200ms-3]"));
+
+  actions.push(logger2Queue2("[Q2-100ms-1]"));
+  actions.push(logger2Queue2("[Q2-100ms-2]"));
+  actions.push(logger2Queue2("[Q2-100ms-3]"));
+
+  actions.push(logger3Queue2("[Q2-100ms-4]"));
+  actions.push(logger3Queue2("[Q2-100ms-5]"));
+  actions.push(logger3Queue2("[Q2-100ms-6]"));
 
   await Promise.all(actions);
   expect(log).toStrictEqual([
@@ -61,4 +73,21 @@ test('RateKeeper Queue', async () => {
     "[Q2-100ms-6]", //500ms
     "[OQ-500ms-3]", //1000ms
   ]);
-})
+});
+
+test('Drop Policy', async () => {
+  const actions: Promise<string>[] = [];
+
+  for (let i = 0; i < 10; i++) {
+    actions.push(logger4Queue3Reject(`Message ${i}`));
+  }
+
+  await Promise.allSettled(actions);
+  expect(log).toStrictEqual([
+    "Message 0",
+    "Message 1",
+    "Message 2",
+    "Message 3",
+    "Message 4",
+  ]);
+});
